ec483c78cbcf9c365a63310c129d3e8e0657332b
Riley Gregg <rtgregg93@gmail.com>
2022-07-22 18:00:11 -0400

    created main containers...to still create smaller boxes

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 // set variables
-let container = document.querySelector("#container");
+let container = document.querySelector(".container-main");
 let rows = document.querySelector(".rows");
 let columns = document.querySelector(".columns");
+let body = document.querySelector("body");
+
+// create elements
+
+let containerInner = document.createElement("div");
+let rowBoxes = document.createElement("div");
 
 // create default grid size 16 x 16
 function createGrid() {
-    createRow(16);
+    createContainer(16);
     createColumn();
 }
 
 // create function that makes rows based on argument
-function createRow(entry) {
-    for (let i = 0; i < entry; i++) {
-        let rowBoxes = document.createElement("div");
-        rowBoxes.classList.add("row-boxes");
-        container.appendChild(rowBoxes);
+function createContainer() {
+    for (let i = 0; i < 16; i++) {
+        let containerMain = document.createElement("div");
+        containerMain.classList.add("container-main");
+
+        let containerInner = document.createElement("div");
+        containerInner.classList.add("container-inner");
+
+        body.appendChild(containerMain);
+        containerMain.appendChild(containerInner);
     }
 }
+
+createContainer(16);
 
 function createColumn() {
-    for (let i = 0; i < createRow(16); i++) {
-        let columnBoxes = document.createElement("div");
-        columnBoxes.classList.add("column-boxes");
-        container.appendChild(columnBoxes);
+    for (let i = 0; i < 16; i++) {
+        let containerInner = document.createElement("div");
+        rowBoxes.classList.add("row-boxes");
+        rowBoxes.textContent = "box";
+        containerInner.appendChild(rowBoxes);
+
+        createContainer(16);
     }
 }
-
-createRow(16);
-createColumn();
