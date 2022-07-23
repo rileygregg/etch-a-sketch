3f178eebb0b3013337928e51b35e1c5e5c34f196
Riley Gregg <rtgregg93@gmail.com>
2022-07-23 13:33:48 -0400

    created grid uusing css grid and js

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-// set variables
-let container = document.querySelector(".container-main");
-let rows = document.querySelector(".rows");
-let columns = document.querySelector(".columns");
-let body = document.querySelector("body");
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
 
-// create elements
+let cells = document.getElementsByClassName("cell");
 
-let containerInner = document.createElement("div");
-let rowBoxes = document.createElement("div");
-
-// create default grid size 16 x 16
-function createGrid() {
-    createContainer(16);
-    createColumn();
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRow(16);
+    makeColumns(16);
 }
 
-// create function that makes rows based on argument
-function createContainer() {
-    for (let i = 0; i < 16; i++) {
-        let containerMain = document.createElement("div");
-        containerMain.classList.add("container-main");
-
-        let containerInner = document.createElement("div");
-        containerInner.classList.add("container-inner");
-
-        body.appendChild(containerMain);
-        containerMain.appendChild(containerInner);
+// Takes (rows, columns) input and makes a grid
+function makeCells(cellNum) {
+    // Creates rows
+    for (let r = 0; r < cellNum; r++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        container.appendChild(cell).className = "cell";
     }
 }
 
-createContainer(16);
-
-function createColumn() {
-    for (let i = 0; i < 16; i++) {
-        let containerInner = document.createElement("div");
-        rowBoxes.classList.add("row-boxes");
-        rowBoxes.textContent = "box";
-        containerInner.appendChild(rowBoxes);
-
-        createContainer(16);
-    }
-}
+makeCells(256);
